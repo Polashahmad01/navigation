4513cbb0e77f9a37f3f083c673410ab1fa0f01ce
Polashahmad01 <dev.polashahmad@gmai.com>
2020-11-01 11:03:32 +0600

    navigation complete

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
-const Menu = () => {
+const Menu = ({ items }) => {
 
-    return <div>Menu</div>
+    return (
+        <div className="section-center">
+            {items.map(menuItem => {
+                const { id, title, img, des, price  } = menuItem;
+                return (
+                    <article className="menu-item" key={id}>
+                        <img src={img} alt={title}/>
+                        <div className="item-info">
+                            <header>
+                                <h4>{title}</h4>
+                                <h4 className="price">${price}</h4>
+                            </header>
+                                <p className="item-text">{des}</p>
+                        </div>
+                    </article>
+                );
+            })}
+        </div>
+    );
 };
 
 export default Menu; 
